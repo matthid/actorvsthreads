db096b9e066077308d8f1b83b92b83257bc29412
Matthias Dittrich <matthi.d@googlemail.com>
2012-06-19 15:32:01 +0200

    Added F# Counter-Actor Example

--- ActorvsThreads/SheetExamples.fs
+++ ActorvsThreads/SheetExamples.fs
@@ -6,3 +6,28 @@ module LockExample =
         lock lockObject (fun () ->
                 printf "Locked statement %s" (p.ToString())
             )
+
+module ActorExample = 
+    
+    type CounterMessage = 
+        | Stop
+        | Go
+        | Start of AsyncReplyChannel<int>
+    let counter = MailboxProcessor.Start(fun inbox -> 
+        let rec loop count rep =
+            async {
+                let! msg = inbox.Receive()
+                match msg with 
+                | Start (replyChannel) -> return! loop 0 (Some replyChannel)
+                | Go -> return! loop (count+1) rep
+                | Stop -> return ()
+            }
+        loop 0 None)
+
+    let testCounter() =
+        async {
+            let! finished = counter.PostAndAsyncReply(fun rep -> Start(rep));
+            printf "Counter got to %i" finished
+        } |> Async.RunSynchronously
+            
+            

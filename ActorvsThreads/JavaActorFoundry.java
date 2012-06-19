c38cc9ce7987caf684aeacd72b7101fe3ae4c8ea
Matthias Dittrich <matthi.d@googlemail.com>
2012-06-19 16:02:52 +0200

    Added Java ActorFoundry Code

---
+++ ActorvsThreads/JavaActorFoundry.java
@@ -0,0 +1,28 @@
+﻿package actorfoundry.examples.counter;
+import osl.manager.*;
+import osl.util.*;
+import osl.manager.annotations.message;
+
+public class Counter extends Actor {
+
+	private int count = 0;
+  @message public void boot() throws RemoteCodeException {
+    ActorName counter = create("actorfoundry.examples.counter.Counter");
+    send(counter, "Start", counter);
+  }
+
+  @message public void Start(ActorName counter) {
+	count = 0;
+	send(counter, "Go", counter);
+	send(counter, "Stop");
+  }
+  
+  @message public void Go(ActorName counter) {
+	count++;
+	send(counter, "Go", counter);
+  }
+
+  @message public void Stop() {
+    send(stdout, "println",  "Counter got to " + count);
+  }
+}
